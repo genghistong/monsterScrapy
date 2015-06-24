a1796283ef207e943e654b0733ff547989310b93
unknown <julian.k.tong@gmail.com>
2015-06-24 16:48:16 -0400

    added recursive spider but has error

--- monster/spiders/monsterSpi.py
+++ monster/spiders/monsterSpi.py
@@ -1,10 +1,15 @@
 from scrapy.spider import Spider
 from scrapy.selector import Selector
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.http import Request
+from scrapy.selector import *
+import sys
 
 from monster.items import MonsterItem
 
 
-class MonsterSpider(Spider):
+class MonsterSpider(CrawlSpider):
     name = "monster"
     allowed_domains = ["jobs.monster.com"]
     start_urls = [
@@ -30,22 +35,39 @@ class MonsterSpider(Spider):
 
     ]
 
-    def parse(self, response):
-        sel = Selector(response)
-        sites = sel.xpath('//div[@class="col-xs-12"]')
+    rules = (Rule(SgmlLinkExtractor(allow=("jobs.monster.com",))
+         , callback = 'parse_items'),)
+
+    def parse_items(self, response):
+        # sel = Selector(response)
+        # sites = sel.xpath('//div[@class="col-xs-12"]')
+        hxs = HtmlXPathSelector(response)
+        sites = hxs.select('//div[@class="col-xs-12"]')
+        #sites = hxs.select('//article[@class="js_result_row"]')
+
         items = []
 
-        for site in sites.select('..//article[@class="js_result_row"]'):
+        for site in sites.xpath('.//article[@class="js_result_row"]'):
             item = MonsterItem()
             item['title'] = site.xpath('.//span[@itemprop = "title"]/text()').extract()
             item['company'] = site.xpath('.//span[@itemprop = "name"]/text()').extract()
             item['city'] = site.xpath('.//span[@itemprop = "addressLocality"]/text()').extract()
             item['state'] = site.xpath('.//span[@itemprop = "addressRegion"]/text()').extract()
-            item['description'] = site.xpath('.//div[@class = "preview"]/text()').extract()
-            if item not in items:
-                items.append(item)
-            #limiting it to 100 does not work, maybe because it's counting
-            #the length of the site loop which never exceeds 20?
-            #if len(items) > 100:
-            #    break
-        return items
+            item['link'] = site.xpath('.//a[@data-m_impr_a_placement_id="jsr"]/@href').extract()
+
+            # error that comes up is that it needs a string not a list
+            #url = item['link']
+            #doing this shoots a valueerror "missing scheme in request url: %s'"
+            url = ''.format(''.join(item['link']))
+            yield Request(url = url, meta={'item':item}, callback = self.parse_item_page)
+
+    def parse_item_page(self,response):
+        hxs = HtmlXPathSelector(response)
+
+        item = response.meta['item']
+        item['description'] = hxs.select('//div[@itemprop = "description"]/text()').extract()
+            #item['description'] = site.xpath('.//div[@class = "preview"]/text()').extract()
+        # if item not in items:
+        #     items.append(item)
+        return item
+
